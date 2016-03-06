ff629461fb17e8e7e43c32b8ac7193930aa2e83e
Alvin Jonel <alvinj.delacruz@gmail.com>
2016-03-06 13:07:34 -0400

    Create Board class

--- src/app.js
+++ src/app.js
@@ -1,36 +1,82 @@
+var Board = function () {
+    this._turn = 1;
+    this._matrix = [];
+    this._gameWonListeners = [];
+    this._xTurnListeners = [];
+    this._0TurnListeners = [];
+    for (var i = 0; i < this._sides; i++) {
+        this._matrix.push(new Array(3));
+    }
+};
+Board.prototype = cc.extend(Object);
+Board.prototype.mark = function (r, c) {   
+      if (this._matrix[r][c] !== undefined) return;
+      
+      if (this._turn % 2 == 0) {
+          this._matrix[r][c] = true;
+          this._raiseXTurnEvent(r, c);
+      } else {
+          this._matrix[r][c] = false;
+          this._raise0TurnEvent(r, c);
+      } 
+      
+      if (this._isAWinTurn(r, c)){
+          this._raiseGameWonEvent();
+      }
+      
+      this._turn++;
+};
+
+Board.prototype._isAWinTurn = function (r, c) {
+    return false;    
+};
+
+Board.prototype.getWinnerLine = function () {
+   return {};
+};
+
+Board.prototype._raiseGameWonEvent = function (r, c) {
+    var winLine = this.getWinnerLine();
+    this._gameWonListeners.forEach(function (evt) {
+        evt({ first: winLine.first, last: winLine.last}); 
+    });
+};
+
+Board.prototype._raiseXTurnEvent = function (r, c) {
+    this._xTurnListeners.forEach(function (evt) {
+         evt({row: r, col: c});
+    });
+};
+
+Board.prototype._raise0TurnEvent = function (r, c) {
+    this._0TurnListeners.forEach(function (evt) {
+        evt({row: r, col: c}); 
+    });
+};
+
+Board.prototype._sides = 3;
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
+        
         var size = cc.winSize;
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("TicTacToe V0.1", "Arial", 38);
-        // position the label on the center of the screen
+        
+        var helloLabel = new cc.LabelTTF("Tic Tac Toe V0.1", "Arial", 38);
         helloLabel.x = size.width / 2;
         helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
         this.addChild(helloLabel, 5);
 
-        // add "HelloWorld" splash screen"
         this.sprite = new cc.Sprite(res.tablero_png);
         this.sprite.attr({
             x: size.width / 2,
             y: size.height / 2
         });
+        
         this.addChild(this.sprite, 0);
-
+        
+        
         return true;
     }
 });
