00cf6cd6fa9b9028f5f6fc8742f5dd16907c5931
Alvin Jonel <alvinj.delacruz@gmail.com>
2016-03-06 19:59:58 -0400

    Add events which allow add x and o to the board

--- src/app.js
+++ src/app.js
@@ -5,27 +5,33 @@ var Board = function () {
     this._xTurnListeners = [];
     this._0TurnListeners = [];
     for (var i = 0; i < this._sides; i++) {
-        this._matrix.push(new Array(3));
+        this._matrix.push(new Array(this._sides));
     }
 };
 Board.prototype = cc.extend(Object);
 Board.prototype.mark = function (r, c) {   
       if (this._matrix[r][c] !== undefined) return;
+      if ( r >= this._sides && r < 0) return;
+      if ( c >= this._sides && c < 0) return;
       
-      if (this._turn % 2 == 0) {
+      if (this._turn % 2 != 0) {
           this._matrix[r][c] = true;
-          this._raiseXTurnEvent(r, c);
+          this._raiseEvent(this.X_TURN, r, c);
       } else {
           this._matrix[r][c] = false;
-          this._raise0TurnEvent(r, c);
+          this._raiseEvent(this.ZERO_TURN, r, c);
       } 
       
       if (this._isAWinTurn(r, c)){
-          this._raiseGameWonEvent();
+          this._raiseEvent();
       }
       
       this._turn++;
 };
+
+Board.prototype.BOARD_WON = "BOARD_WON_EVENT";
+Board.prototype.X_TURN = "X_TURN";
+Board.prototype.ZERO_TURN = "ZERO_TURN";
 
 Board.prototype._isAWinTurn = function (r, c) {
     return false;    
@@ -35,31 +41,78 @@ Board.prototype.getWinnerLine = function () {
    return {};
 };
 
-Board.prototype._raiseGameWonEvent = function (r, c) {
-    var winLine = this.getWinnerLine();
-    this._gameWonListeners.forEach(function (evt) {
-        evt({ first: winLine.first, last: winLine.last}); 
-    });
-};
-
-Board.prototype._raiseXTurnEvent = function (r, c) {
-    this._xTurnListeners.forEach(function (evt) {
-         evt({row: r, col: c});
-    });
-};
-
-Board.prototype._raise0TurnEvent = function (r, c) {
-    this._0TurnListeners.forEach(function (evt) {
-        evt({row: r, col: c}); 
-    });
+Board.prototype._raiseEvent = function (eventType, r, c) {
+    var evt = new cc.EventCustom(eventType);
+    evt.setUserData({r: r, c: c});
+    cc.eventManager.dispatchEvent(evt);
 };
 
 Board.prototype._sides = 3;
+
+function mapTouchToIndexes (x, y) {
+    var w = 95, 
+        h = 100;
+    var origin = { x: 346, y: 417 };
+    
+    return {
+        row: Math.floor((origin.y - y)/h),
+        col: Math.floor((x - origin.x)/w)
+    }
+}
+
+function mapIndexesToCoord(indexes) {
+    return {
+        x: 95*indexes.c + 346 + 95/2,
+        y:  417 - 100*indexes.r - 100/2
+    }
+}
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
         this._super();
+        
+        this.board = new Board();
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            onTouchBegan: function (touch, event) {	
+                var p = touch._point;
+                var indexes;
+                if (p.x - 346 <= 3*95 && p.x - 346 > 0 &&
+                    417 - p.y <= 3*100 && 417 - p.y > 0) {
+                    indexes = mapTouchToIndexes(touch._point.x, touch._point.y);
+                    self.board.mark(indexes.row, indexes.col);
+                }
+            },
+        }, this);
+        
+        var xTurnListener = cc.EventListener.create({
+            event: cc.EventListener.CUSTOM,
+            eventName: Board.prototype.X_TURN,
+            callback: function (event) {
+                var indexes = event.getUserData();
+                
+                var xSprite = new cc.Sprite(res.X_png);
+                xSprite.attr(mapIndexesToCoord(indexes));
+                self.addChild(xSprite);
+            }
+        });
+        cc.eventManager.addListener(xTurnListener, 1);
+        
+        var zTurnListener = cc.EventListener.create({
+            event: cc.EventListener.CUSTOM,
+            eventName: Board.prototype.ZERO_TURN,
+            callback: function (event) {
+                var indexes = event.getUserData();
+                
+                var ySprite = new cc.Sprite(res.O_png);
+                ySprite.attr(mapIndexesToCoord(indexes));
+                self.addChild(ySprite);
+            }
+        });
+        cc.eventManager.addListener(zTurnListener, 1);
+        
         
         var size = cc.winSize;
         
@@ -68,14 +121,13 @@ var HelloWorldLayer = cc.Layer.extend({
         helloLabel.y = size.height / 2 + 200;
         this.addChild(helloLabel, 5);
 
-        this.sprite = new cc.Sprite(res.tablero_png);
-        this.sprite.attr({
+        this.boardSprite = new cc.Sprite(res.tablero_png);
+        this.boardSprite.attr({
             x: size.width / 2,
             y: size.height / 2
         });
         
-        this.addChild(this.sprite, 0);
-        
+        this.addChild(this.boardSprite, 0);
         
         return true;
     }
@@ -88,4 +140,5 @@ var HelloWorldScene = cc.Scene.extend({
         this.addChild(layer);
     }
 });
+
 
